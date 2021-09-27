cc2ea441de59d1a05d4eb9d644b74cb6037c6415
Naveduran <naveduran@gmail.com>
2021-09-27 04:20:13 +0000

    [Add] Full filters fuctioning!

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,53 +1,66 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   // check api status
-  // $.get('http://127.0.0.1:5001/api/v1/status/', function (data, status) {
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+  $.get('http://127.0.0.1:5001/api/v1/status/', function (data, status) {
+  // $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
     if (status === 'success') {
       $('div#api_status').addClass('available');
     } else { $('div#api_status').removeClass('available'); }
   });
 
   // show amenities list dynamicaly
-  const dict = {};
-  $('.amenities input[type=checkbox]').on('click', function () {
-    const amenityId = $(this).attr('data-id');
-    const amenityName = $(this).attr('data-name');
+  const general = [];
+  const dictAmenities = {};
+  const dictCities = {};
+  const dictStates = {};
 
-    if (dict[amenityName]) {
-      delete dict[amenityName];
+  $('.amenities input[type=checkbox]').on('click', function () {
+    const id = $(this).attr('data-id');
+    const name = $(this).attr('data-name');
+    if (dictAmenities[name]) {
+      delete dictAmenities[name];
     } else { // delete the id of the amenity
-      dict[amenityName] = amenityId;
+      dictAmenities[name] = id;
+      general.push(name);
     }
     let text = '';
-    for (let i = 0; Object.entries(dict)[i]; i++) {
+    for (let i = 0; Object.entries(dictAmenities)[i]; i++) {
       if (i > 0) { text += ', '; }
-      text += Object.entries(dict)[i][0];
+      text += Object.entries(dictAmenities)[i][0];
     }
     $('.amenities h4').text(text);
   });
-    // show states and places list dynamically
-    const dict_2 = {};
-  $('.locations input[type=checkbox]').on('click', function () {
-      console.log('entra')
-      const amenityId = $(this).attr('data-id');
-    const amenityName = $(this).attr('data-name');
 
-    if (dict_2[amenityName]) {
-      delete dict_2[amenityName];
+  $('.st').on('click', function () {
+    const id = $(this).attr('data-id');
+    const name = $(this).attr('data-name');
+    if (dictStates[name]) {
+      delete dictStates[name];
+      let index = general.indexOf(name);
+      general.splice(index, 1);
     } else { // delete the id of the amenity
-      dict_2[amenityName] = amenityId;
+      dictStates[name] = id;
+      general.push(name);
     }
-    let text = '';
-    for (let i = 0; Object.entries(dict_2)[i]; i++) {
-      if (i > 0) { text += ', '; }
-      text += Object.entries(dict_2)[i][0];
+    $('.locations h4').text(general.join(', '));
+  });
+
+  $('.ci').on('click', function () {
+    const id = $(this).attr('data-id');
+    const name = $(this).attr('data-name');
+    if (dictCities[name]) {
+      delete dictCities[name];
+      let index = general.indexOf(name);
+      general.splice(index, 1);
+    } else { // delete the id of the amenity
+      dictCities[name] = id;
+      general.push(name);
     }
-    $('.locations h4').text(text);
+    $('.locations h4').text(general.join(', '));
   });
 
   $.ajax({
-    // url: 'http://127.0.0.1:5001/api/v1/places_search/',
-    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    url: 'http://127.0.0.1:5001/api/v1/places_search/',
+    // url: 'http://0.0.0.0:5001/api/v1/places_search/',
     headers: { 'Content-Type': 'application/json' },
     method: 'POST',
     data: JSON.stringify({}),
@@ -64,5 +77,32 @@ document.addEventListener('DOMContentLoaded', (event) => {
         $('.places').append(text);
       }
     }
+  });
+
+  $('button').on('click', function () {
+    $.ajax({
+      url: 'http://127.0.0.1:5001/api/v1/places_search/',
+      // url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      data: JSON.stringify({ 'amenities': Object.values(dictAmenities), 'cities': Object.values(dictCities), 'states': Object.values(dictStates) }),
+      success: function (data2) {
+        let html = '';
+        for (let i = 0; Object.entries(data2)[i]; i++) {
+          let text = '<article> <div class="title_box"><h2>' + data2[i].name +
+                      '</h2><div class="price_by_night">$' + data2[i].price_by_night +
+                      '</div></div><div class="information"><div class="max_guest">' +
+                      data2[i].max_guest + ' Guest';
+          if (data2[i].max_guest !== 1) { text += 's'; }
+          text += '</div><div class="number_rooms">' + data2[i].number_rooms + ' Bedroom';
+          if (data2[i].number_rooms !== 1) { text += 's'; }
+          text += '</div><div class="number_bathrooms">' + data2[i].number_bathrooms + ' Bathroom';
+          if (data2[i].number_bathrooms !== 1) { text += 's'; }
+          text += '</div></div><div class="description">' + data2[i].description + '</div></article>';
+          html += text;
+        }
+        $('.places').html(html);
+      }
+    });
   });
 });
